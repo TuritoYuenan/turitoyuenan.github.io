75b313d4fc33a665539a4ae1e718930f04be7048
Turito Yuenan <trietntm@hotmail.com>
2022-02-05 14:02:31 +0700

    minor update

--- tools/SclCelGen.js
+++
@@ -1,7 +0,0 @@
-function scl(color, net, url) {
-	var cell = document.createElement("A");
-	cell.href = "https://"+url;
-	cell.style.backgroundColor = color;
-	cell.classList.add("bi-" + net);
-	document.getElementById("box-scl").appendChild(cell);
-}
